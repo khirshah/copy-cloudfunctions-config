15c6b4eb44ed3bdbf38f79d30704e81a09445c0d
Agi <33101589+khirshah@users.noreply.github.com>
2018-01-09 16:41:11 +0000

    chore: refactoring (#1)

--- convert_nest_dict.py
+++ convert_nest_dict.py
@@ -1,39 +1,56 @@
+#----------------------------- import ----------------------------------
 import json
 import argparse
+
+#----------------------------- init ------------------------------------
+#parsing the arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--file',
                         help='filename without extension',
                         required='True')
 args = parser.parse_args()
 
+#open the data file, extract the data and close the file
 with open(args.file+'.json') as f:
     data = f.read()
 d = json.loads(data)
-#print(len(d['content']['github']))
-#print(type(d))
 
+#opening the file we wanna write into
 tx=open(args.file+'.sh','w')
 tx.write('#!/bin/bash\n')
 path=[]
 
-#define function to iterate trough our nested dictionary and write the value-key pairs into a txt
+#--------------------------- functions ---------------------------------
+#define function to iterate trough our nested dictionary
+# and write the value-key pairs into a txt
 def dict_to_list(dc):
-  itemCount=len(dc) #to track where we are and indicate the last item
+  #to track where we are and indicate the last item
+  itemCount=len(dc)
   for i in dc: 
     itemCount-=1
-    if isinstance(dc[i], dict): #if our item is a dictionary, we have to dig deeper
-      path.append(i)  #before going down, let's add the current level to the path
-      dict_to_list(dc[i]) #the function calls itself      
-    elif isinstance(dc[i], str): #else we got a string, that we can write in the file
-      string='firebase functions:config:set '               
-      for item in path:         #we iterate through the list containing the full path, getting the items out one by one
+    #if our item is a dictionary, we have to dig deeper
+    if isinstance(dc[i], dict):
+    #before going down, let's add the current level to the path
+      path.append(i)
+      #the function calls itself
+      dict_to_list(dc[i])
+    #else we got a string, that we can write in the file
+    elif isinstance(dc[i], str):
+      string='firebase functions:config:set '
+      #we iterate through the list containing the full path,
+      #getting out the items one by one              
+      for item in path:
         string=string+item+'.'  
       string=string+i+'='+'\"'+dc[i]+'\"'+'\n'
       tx.write(string)
-      if itemCount==0: #after the last item in the current dictionary we jump one level up, thus we have to delete the last item from our path list
+      #after the last item in the current dictionary we jump one level up,
+      #thus we have to delete the last item from our path list
+      if itemCount==0: 
         path.pop()
 
 
+#------------------------------ commands ---------------------------------
 #call the function and supply d as input dictionary
 dict_to_list(d)
+#close the file we were writing into
 tx.close()
